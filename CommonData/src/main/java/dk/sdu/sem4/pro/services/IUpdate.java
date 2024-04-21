23e6b79e2e28df93124f0c814a51b9f65f94583a
Helge Baad Poulsen <hepou22@student.sdu.dk>
2024-04-21 02:42:17 +0200

    added all update method

--- CommonData/src/main/java/dk/sdu/sem4/pro/services/IUpdate.java
+++ CommonData/src/main/java/dk/sdu/sem4/pro/services/IUpdate.java
@@ -2,12 +2,15 @@ package dk.sdu.sem4.pro.services;
 
 import dk.sdu.sem4.pro.data.*;
 
+import java.io.IOException;
+
 public interface IUpdate {
-    public boolean updateBatch (Batch batch);
-    public boolean updateLogline (int batchID, Logline logline);
-    public boolean updateComponent (Component component);
-    public boolean updateProduct (Recipe recipe);
-    public boolean updateUnit (Unit unit);
-    public boolean updateAGV (AGV agv);
-    public boolean updateUser (User user);
+    public boolean updateBatch (Batch batch) throws IOException;
+    public boolean updateLogline (Logline logline) throws IOException;
+    public boolean updateComponent (Component component) throws IOException;
+    public boolean updateProduct (Recipe recipe) throws IOException;
+    public boolean updateUnit (Unit unit) throws IOException;
+    public boolean updateAGV (AGV agv) throws IOException;
+    public boolean updateUser (User user) throws IOException;
+    public boolean updateUserGroup (UserGroup userGroup) throws IOException;
 }
