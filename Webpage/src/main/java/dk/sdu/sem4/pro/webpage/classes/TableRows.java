0637bd72a9fef187534cd6bfeab848402d5e3ecc
Helge Baad Poulsen <hepou22@student.sdu.dk>
2024-04-29 17:22:32 +0200

    added new class ofr table

---
+++ Webpage/src/main/java/dk/sdu/sem4/pro/webpage/classes/TableRows.java
@@ -0,0 +1,5 @@
+package dk.sdu.sem4.pro.webpage.classes;
+
+public class TableRows {
+
+}
--- Webpage/src/main/java/dk/sdu/sem4/pro/webpage/controller/ReportsController.java
+++ Webpage/src/main/java/dk/sdu/sem4/pro/webpage/controller/ReportsController.java
@@ -3,7 +3,6 @@ package dk.sdu.sem4.pro.webpage.controller;
 import dk.sdu.sem4.pro.commondata.data.Batch;
 import dk.sdu.sem4.pro.commondata.data.Logline;
 import dk.sdu.sem4.pro.commondata.services.ISelect;
-import dk.sdu.sem4.pro.webpage.classes.ReportHeaders;
 import dk.sdu.sem4.pro.webpage.classes.ReportTable;
 import dk.sdu.sem4.pro.webpage.serviceloader.DatabaseLoader;
 import org.springframework.stereotype.Controller;
@@ -12,7 +11,7 @@ import org.springframework.ui.Model;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ServiceLoader;
+import java.util.Map;
 
 @Controller
 public class ReportsController {
@@ -27,17 +26,49 @@ public class ReportsController {
             throw new RuntimeException(e);
         }
         List<ReportTable> reportTables = new ArrayList<>();
-        List<ReportHeaders> headers = new ArrayList<>();
-
+        List<String> headers = new ArrayList<>();
+        headers.add("Product");
+        headers.add("Amount");
+        headers.add("Description")
+        ReportTable reportTable = null;
         for (Batch batch : batches) {
-            ReportTable reportTable = new ReportTable();
-            reportTable.setAmount(batch.getAmount());
-            reportTable.setDescription(batch.getDescription());
-            reportTable.setId(batch.getId());
-            reportTable.setPriority(batch.getPriority());
-            reportTable.setProduct(batch.getProduct().getProduct().getName());
+            reportTable = new ReportTable();
+            if(batch.getId() > 0) {
+                reportTable.setId(batch.getId());
+                if(!headers.contains("ID")) {
+                    headers.add("ID");
+                }
+            }
+            if(batch.getProduct().getProduct().getName() != null) {
+                reportTable.setProduct(batch.getProduct().getProduct().getName());
+                if(!headers.contains("Product")) {
+                    headers.add("Product");
+                }
+            }
+            if(batch.getAmount() > 0) {
+                reportTable.setAmount(batch.getAmount());
+                if(!headers.contains("Amount")) {
+                    headers.add("Amount");
+                }
+            }
+            if(batch.getDescription() != null) {
+                reportTable.setDescription(batch.getDescription());
+                if(!headers.contains("Description")) {
+                    headers.add("Description");
+                }
+                headers.add("Description");
+            }
+            if(reportTable.getPriority() >= 0) {
+                reportTable.setPriority(batch.getPriority());
+                if(!headers.contains("Priority")) {
+                    headers.add("Priority");
+                }
+            }
             for(Logline logline : batch.getLog()) {
                 reportTable.getLoglines().add(logline);
+                if(!headers.contains(logline.getType())) {
+                    headers.add(logline.getType());
+                }
             }
             reportTables.add(reportTable);
         }
