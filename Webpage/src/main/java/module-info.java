84ec95d0c6d3f6c5446078c8267fd16441afa7a6
Karl Johan Thomsen <kthom20@student.sdu.dk>
2024-04-29 18:35:10 +0200

    Added module info and experimented with different pom.xml settings

---
+++ Webpage/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Webpage {
+    requires CommonData;
+    requires Common;
+    requires spring.context;
+    requires spring.web;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+}
