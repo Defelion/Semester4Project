97732f33e8f2d6c618ede5d5f72669ffb03b85ed
Karl Johan Thomsen <kthom20@student.sdu.dk>
2024-04-30 11:56:13 +0200

    FIXED NO RUN THING and renamed communication to Communication

--- Webpage/src/main/java/module-info.java
+++ Webpage/src/main/java/module-info.java
@@ -1,8 +1,11 @@
-module Webpage {
+open module Webpage {
     requires CommonData;
     requires Common;
     requires spring.context;
     requires spring.web;
     requires spring.boot;
     requires spring.boot.autoconfigure;
+    requires java.sql;
+    requires spring.core;
+    requires spring.beans;
 }
