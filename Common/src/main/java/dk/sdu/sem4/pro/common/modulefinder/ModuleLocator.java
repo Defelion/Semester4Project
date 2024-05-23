f281db7cd353ba3a4999c2260eb3e2707dc96559
Helge Baad Poulsen <hepou22@student.sdu.dk>
2024-05-23 16:42:46 +0200

    Revert "removed compiler args because it is not needed anymore"
    This reverts commit 90a8ce8793d1efb021ea53104bbf660d0b89813b.

--- Common/src/main/java/dk/sdu/sem4/pro/common/modulefinder/ModuleLocator.java
+++ Common/src/main/java/dk/sdu/sem4/pro/common/modulefinder/ModuleLocator.java
@@ -24,20 +24,23 @@ public abstract class ModuleLocator {
 
             Predicate<Path> filter = path -> path.getFileName().toString().endsWith("SNAPSHOT.jar");
             Stream<Path> modulesStream = Files.list(modulesDir).filter(filter);
+
+            //Stream<Path> modulesStream = Files.find(modulesDir, 1, (path, attr) -> filter.test(path));
+
+            //ModuleFinder finder = ModuleFinder.of(modulesDir);
+            /*List<String> modules = finder.findAll()
+                    .stream()
+                    .map(ModuleReference::descriptor)
+                    .map(ModuleDescriptor::name)
+                    .collect(Collectors.toList());*/
                 List<String> modules = modulesStream
                         .map(ModuleLocator::moduleNameFromJar)
-                        .distinct()
                         .collect(Collectors.toList());
             for(String module : modules) {
                 System.out.println(module);
             }
 
-            Configuration configuration = ModuleLayer.boot()
-                    .configuration()
-                    .resolve(
-                            ModuleFinder.of(modulesDir),
-                            ModuleFinder.of(),
-                            modules);
+            Configuration configuration = ModuleLayer.boot().configuration().resolve(ModuleFinder.of(modulesDir),ModuleFinder.of(),modules);
             return ModuleLayer.boot().defineModulesWithOneLoader(configuration, ClassLoader.getSystemClassLoader());
         } catch (IOException e) {
             System.out.println(e);
@@ -48,7 +51,7 @@ public abstract class ModuleLocator {
     private static String moduleNameFromJar(Path path) {
         try (FileSystem jarFile = FileSystems.newFileSystem(path, (ClassLoader)null)) {
             String jarfileName = path.getFileName().toString().substring(0, path.getFileName().toString().indexOf("-"));
-            //System.out.println(jarfileName);
+            System.out.println(jarfileName);
             return jarfileName;
         } catch (IOException e) {
             System.out.println(e);
